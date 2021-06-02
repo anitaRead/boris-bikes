acbc5d5028997450e3d61b8abe57d7e950c46542
anitaRead <areadman91@gmail.com>
2021-06-02 13:34:39 +0100

    Remove comments

--- spec/boris-bikes_spec.rb
+++ spec/boris-bikes_spec.rb
@@ -1,8 +1,5 @@
 require 'boris-bikes'
 
 describe DockingStation do
-  # it 'release a bike from docking station' do
-  #   docking_station = DockingStation.new
-  #   expect(docking_station).respond_to?(release_bike)
-  # end
+  
 end
